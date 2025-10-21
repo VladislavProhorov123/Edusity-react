69f0cee697bec1e0fe3d9952cf1e7135b36ad162
Vladislav <vladislavprokhorov1005@gmail.com>
2025-10-21 20:19:41 +0300

    the Campus section has been created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Hero from './Components/Hero/Hero'
 import Programs from './Components/Programs/Programs'
 import Title from './Components/Title/Title'
 import About from './Components/About/About'
+import Campus from './Components/Campus/Campus'
 
 export default function App() {
   return (
@@ -14,6 +15,8 @@ export default function App() {
         <Title subTitle='Our PROGRAM' title='What We Offer' />
         <Programs />
         <About />
+        <Title subTitle='Gallery' title='Campus Photos' />
+        <Campus />
       </div>
       
     </div>
